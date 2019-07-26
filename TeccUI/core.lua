b3aca09ccc4622ebf8ac840a30a00d4003a7d2cb
Dennis <icemansparks@gmail.com>
2019-07-26 15:16:28 +0200

    add basic slash commands

--- TeccUI/core.lua
+++ TeccUI/core.lua
@@ -1,3 +1,18 @@
-## some code
+-- some code
+
+-- basic slash commands
+SLASH_RELOADUI1 = "/rl"
+Slash.CmdList.RELOADUI = ReloadUI
+
+SLASH_FRAMESTK1 = "/fs"
+SlashCmdList.FRAMESTK = function()
+	LoadAddOn('Blizzard_DebugTools')
+	FrameStackTooltip_Toggle()
+end
+
+for i = 1, NUM_CHAT_WINDOWS do
+	_G["ChatFrame"..i.."EditBox"]:SetAltArrowKeyMode(false)
+end
+
 
 message('Hello World!')
